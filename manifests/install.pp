1362525c0b76259485c5eb77d0b0a4fe2ac4ed8a
James Roberts <james.roberts@itv.com>
2019-03-12 18:37:15 +0000

    Add full paths to tools

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,7 +4,7 @@ class anaconda::install {
 
     # Bug fix: the require will force a download even if it doesn't have to be installed
     exec { "install_anaconda":
-        command => "bash /tmp/${anaconda::params::installer} -b -p /opt/anaconda/eda",
+        command => "/usr/bin/bash /tmp/${anaconda::params::installer} -b -p /opt/anaconda/eda",
         creates => "/opt/anaconda/eda",
         require => Exec["download_anaconda"],
     }
@@ -13,9 +13,9 @@ class anaconda::install {
     # when it doesn't need to be installed
     # Avoid circular dependency on "install_anaconda", run if /opt/anaconda doesn't exist
     exec { "download_anaconda":
-        command => "wget -P /tmp ${anaconda::params::url}",
+        command => "/usr/bin/wget -P /tmp ${anaconda::params::url}",
         creates => "/tmp/${anaconda::params::installer}",
-        onlyif  => "test ! -d /opt/anaconda/eda",
+        onlyif  => "/usr/bin/test ! -d /opt/anaconda/eda",
     }
 
     file { "/opt/anaconda/eda/.condarc":
